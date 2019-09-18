33500d6df43bfecfc93b44dac69daadcee9d4fdd
Adrian Gygax <adrian.gygax@visana.ch>
2019-09-18 13:23:40 +0200

    Make library compatible with IE 11

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
     entry: {
-        htmldiff: ['./src/Diff.js'],
+        htmldiff: ['babel-polyfill', './src/Diff.js'],
     },
 
     output: {
@@ -19,9 +19,11 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader'
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                }
             }
         ]
     },
